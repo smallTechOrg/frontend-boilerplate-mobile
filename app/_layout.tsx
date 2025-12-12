29e65bc332321175a210a49e6660feeb4ea0ad16
prachinayak1509 <prachinayak1509@gmail.com>
2025-12-12 15:00:53 +0530

    navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -21,13 +21,13 @@ export default function RootLayout() {
           title: "Home",
         }}
       />
-      <Drawer.Screen
+       <Drawer.Screen
         name="(tabs)"
         options={{
-          title: "Main",
-          headerShown: true,
+         drawerItemStyle: { display: "none" }  
         }}
       />
+
       <Drawer.Screen
         name="details"
         options={({ navigation }) => ({
