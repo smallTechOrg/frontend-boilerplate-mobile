fd9f444ca478af2da652caffbf0152f7bb57c724
Prachi Nayak <prachinayak1509@gmail.com>
2025-12-15 11:14:23 +0530

    Navigation + Layout (#3)
    * Reset project and added basic navigation
    
    * Added Headers
    
    * bottom tabs
    
    * nav on root index.tsx
    
    * removed styling
    
    * details - back navigation
    
    * navigation
    
    * styling removed
    
    ---------
    
    Co-authored-by: Sai Krishna Vennamaneni <saivnm5@gmail.com>

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,37 @@
-import { Stack } from "expo-router";
+// app/_layout.tsx
+import { HeaderBackButton } from "@react-navigation/elements";
+import { Drawer } from "expo-router/drawer";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Drawer
+      initialRouteName="(tabs)"
+      screenOptions={{
+        drawerActiveTintColor: 'blue', // Color for the active link in the drawer menu
+      }}
+    >
+      <Drawer.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+       <Drawer.Screen
+        name="(tabs)"
+        options={{
+         drawerItemStyle: { display: "none" }  
+        }}
+      />
+
+      <Drawer.Screen
+        name="about"
+        options={({ navigation }) => ({
+          title: "About Page",
+          headerLeft: (props) => (
+            <HeaderBackButton {...props} onPress={() => navigation.goBack()} />
+          ),
+        })}
+      />
+    </Drawer>
+  );
 }
